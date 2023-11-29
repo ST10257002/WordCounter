31b09b0754bba2d56aef62ea959fe29e79a4ea31
ST10257002 <ST10257002@vcconnect.edu.za>
2023-11-29 09:39:34 +0200

    Optimise code structure

--- src/app/App.java
+++ src/app/App.java
@@ -17,6 +17,8 @@ import javax.swing.JOptionPane;
 
 public class App extends javax.swing.JFrame {
     
+    private static final String fileConfig = "src/app/config/app.properties";
+    
     private static final Properties property = new Properties();
 
     public App() {
@@ -24,7 +26,7 @@ public class App extends javax.swing.JFrame {
     }
 
     public static void main(String args[]) {
-        try (InputStream stream = new FileInputStream("src/app/config/app.properties")) {
+        try (InputStream stream = new FileInputStream(fileConfig)) {
             property.load(stream);
             // Select theme from properties configs
             switch (property.getProperty("windowTheme")) {
@@ -236,7 +238,7 @@ public class App extends javax.swing.JFrame {
 
         groupThemes.add(themeDark);
         themeDark.setIcon(new javax.swing.ImageIcon(getClass().getResource("/app/assets/icons8-moon-18.png"))); // NOI18N
-        themeDark.setSelected(defaultOf_ThemeDark());
+        themeDark.setSelected(getDefaultOfThemeDark());
         themeDark.setToolTipText("Dark theme");
         themeDark.setFocusable(false);
         themeDark.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
@@ -250,7 +252,7 @@ public class App extends javax.swing.JFrame {
 
         groupThemes.add(themeLight);
         themeLight.setIcon(new javax.swing.ImageIcon(getClass().getResource("/app/assets/icons8-light-18.png"))); // NOI18N
-        themeLight.setSelected(defaultOf_ThemeLight());
+        themeLight.setSelected(getDefaultOfThemeLight());
         themeLight.setToolTipText("Light theme");
         themeLight.setFocusable(false);
         themeLight.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
@@ -448,18 +450,6 @@ public class App extends javax.swing.JFrame {
     private void dConfigConfirmActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_dConfigConfirmActionPerformed
         // Write properties to the config file
         toConfig("timeFormat", String.valueOf(dConfigTimeCombo.getSelectedIndex()));
-        /*
-        String path = "src/app/config/app.properties";
-        try (InputStream stream = new FileInputStream(path);
-            OutputStream output = new FileOutputStream(path)) 
-        {
-            property.load(stream);
-            property.setProperty("timeFormat", String.valueOf(dConfigTimeCombo.getSelectedIndex()));
-            property.store(output, null);
-        } catch (IOException e) {
-            System.err.print(e);
-        }
-        */
     }//GEN-LAST:event_dConfigConfirmActionPerformed
 
     private void themeLightActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_themeLightActionPerformed
@@ -479,7 +469,7 @@ public class App extends javax.swing.JFrame {
     }
     
     private String readConfig(String key) {
-        String path = "src/app/config/app.properties";
+        String path = fileConfig;
         try (InputStream stream = new FileInputStream(path)) {
             property.load(stream);
             return property.getProperty(key);
@@ -490,7 +480,7 @@ public class App extends javax.swing.JFrame {
     }
     
     private void toConfig(String key, String value) {
-        String path = "src/app/config/app.properties";
+        String path = fileConfig;
         try (InputStream stream = new FileInputStream(path);
             OutputStream output = new FileOutputStream(path)) 
         {
@@ -502,24 +492,11 @@ public class App extends javax.swing.JFrame {
         }
     }
     
-    private void setDefaultStates() {
-        switch (readConfig("windwoTheme")) {
-            case ("light") -> {
-                this.themeLight.setSelected(true);
-                this.themeDark.setSelected(false);
-            }
-            case ("dark") -> {
-                this.themeLight.setSelected(false);
-                this.themeDark.setSelected(true);
-            }
-        }
-    }
-    
-    private boolean defaultOf_ThemeLight() {
+    private boolean getDefaultOfThemeLight() {
         return ("light".equals(readConfig("windowTheme")));
     }
     
-    private boolean defaultOf_ThemeDark() {
+    private boolean getDefaultOfThemeDark() {
         return ("dark".equals(readConfig("windowTheme")));
     }
     
