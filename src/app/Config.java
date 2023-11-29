0bc3d46dfb3c82ab7405cdd2ccb01da2bba097ff
ST10257002 <ST10257002@vcconnect.edu.za>
2023-11-29 15:20:04 +0200

    Update configuration file handler class

--- src/app/Config.java
+++ src/app/Config.java
@@ -7,15 +7,33 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 import java.util.ArrayList;
 import java.util.Properties;
 import java.util.stream.Stream;
 
+/**
+ * Class to handle interactions with configuration files.
+ * 
+ * @author matthew-pieterse
+ */
+
 public class Config {
+    
+    // Directory containing all application configuration files
+    // C:\Users\Username\AppData\Roaming\WordCounter
+    
+    static final Path directory = Paths.get(
+            System.getProperty("user.home"),
+            "AppData",
+            "Roaming",
+            "WordCounter"
+    );
     
     /**
      * Update the local configuration file with the packaged template.
@@ -34,10 +52,8 @@ public class Config {
             }
         }
 
-        Path targetPath = Paths.get(System.getProperty("user.home"), "AppData", "Roaming", "WordCounter");
-
         for (Path path : configFiles) {
-            Path filePath = targetPath.resolve(path.getFileName());
+            Path filePath = directory.resolve(path.getFileName());
             if (!Files.exists(filePath)) {
                 Files.createDirectories(filePath.getParent());
                 Files.copy(path, filePath);
@@ -50,7 +66,7 @@ public class Config {
      * 
      * @param config
      * @param key
-     * @return
+     * @return value
      * @throws IOException 
      */
     
@@ -60,6 +76,27 @@ public class Config {
         Properties property = new Properties();
         
         try (InputStream stream = new FileInputStream(config)) {
+            property.load(stream);
+        }
+        return property.getProperty(key);
+    }
+    
+    /**
+     * Returns the value of a property in the specified file within the configurations folder.
+     * 
+     * @param filename
+     * @param key
+     * @return value
+     * @throws IOException 
+     */
+    
+    public static String getProperty(String filename, String key)
+      throws IOException {
+        
+        Properties property = new Properties();
+        Path path = directory.resolve(filename);
+        
+        try (InputStream stream = Files.newInputStream(path)) {
             property.load(stream);
         }
         return property.getProperty(key);
@@ -86,6 +123,33 @@ public class Config {
         }
 
         try (OutputStream outputStream = new FileOutputStream(config)) {
+            property.setProperty(key, value);
+            property.store(outputStream, null);
+        }
+    }
+    
+    /**
+     * Sets the value of a property in the specified file within the configurations folder.
+     * 
+     * @param filename
+     * @param key
+     * @param value
+     * @throws IOException 
+     */
+    
+    public static void setProperty(String filename, String key, String value)
+      throws IOException {
+        
+        Properties property = new Properties();
+        Path path = directory.resolve(filename);
+        
+        // The try-with-lease blocks are separated to avoid data loss
+        
+        try (InputStream inputStream = Files.newInputStream(path)) {
+            property.load(inputStream);
+        }
+
+        try (OutputStream outputStream = Files.newOutputStream(path)) {
             property.setProperty(key, value);
             property.store(outputStream, null);
         }
