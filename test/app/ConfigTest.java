d46e1497e1eeb6f97154b7b663c8e860665a3479
ST10257002 <ST10257002@vcconnect.edu.za>
2023-11-29 19:23:03 +0200

    Attempt to track test files

---
+++ test/app/ConfigTest.java
@@ -0,0 +1,48 @@
+
+package app;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * JUnit testing class.
+ * 
+ * @author 1matt
+ */
+
+public class ConfigTest {
+    
+    @BeforeEach
+    public void setUp() {
+        
+    }
+    
+    // -- Tests
+
+    @Test
+    public void testGetProperty_File() 
+      throws Exception {
+        
+        //
+    }
+
+    @Test
+    public void testGetProperty_Filename() 
+      throws Exception {
+        
+        //
+    }
+
+    @Test
+    public void testSetProperty_File() 
+      throws Exception {
+        
+        //
+    }
+
+    @Test
+    public void testSetProperty_Filename() 
+      throws Exception {
+        
+        //
+    }
+}
